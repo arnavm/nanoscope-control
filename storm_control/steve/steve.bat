c0d8ad3d050a9a4a7b12a6fb0fd49165db0707f2
Arnav <ocean57@gmail.com>
2024-05-15 23:09:44 -0700

    Updated to keep command prompt open in case of error

--- storm_control/steve/steve.bat
+++ storm_control/steve/steve.bat
@@ -1,4 +1,1 @@
-rem Activate virtual environment and start Steve.
-call C:\Users\Scope4\anaconda3\Scripts\activate.bat C:\Users\Scope4\anaconda3\ && activate Hal2 && python steve.py
-
-
+call C:\Users\Scope5\AppData\Local\anaconda3\Scripts\activate.bat C:\Users\Scope5\AppData\Local\anaconda3\ && activate scope5 && python steve.py || cmd /k
